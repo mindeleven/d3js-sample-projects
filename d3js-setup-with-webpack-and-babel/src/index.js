d8c5fb04cc66bf1ef2a3659c827479410b42e261
Jürgen Kober <mindeleven@posteo.de>
2020-04-04 15:03:24 +0200

    Initial setup fa a D3 sample project.

--- d3js-setup-with-webpack-and-babel/src/index.js
+++ d3js-setup-with-webpack-and-babel/src/index.js
@@ -1,6 +1,22 @@
 import * as d3 from 'd3';
 
-console.log("working");
+d3.csv('../src/data/tour_de_france.csv')
+  .then(dataIsReady);
 
-const square = d3.selectAll("rect");
-square.style("fill", "orange");
+function dataIsReady(data) {
+  updateChart(data);
+}
+
+function updateChart(eventData) {
+  d3.select('svg g.chart')
+    .selectAll('circle')
+    .data(eventData)
+    .join('circle')
+    .attr('cx', function(d, i) {
+      return i * 5;
+    })
+    .attr('cy', 50)
+    .attr('r', function(d) {
+      return +d.Entrants;
+    })
+}
