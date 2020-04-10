9f1f79b36427d7bce7af8167a1a6d0aeaa6a89f6
Jürgen Kober <mindeleven@posteo.de>
2020-04-10 14:56:25 +0200

    A first approximation for printing the data as circles on the screen.

--- d3js-setup-with-webpack-and-babel/src/index.js
+++ d3js-setup-with-webpack-and-babel/src/index.js
@@ -3,20 +3,50 @@ import * as d3 from 'd3';
 d3.csv('../src/data/tour_de_france.csv')
   .then(dataIsReady);
 
+let maxRadius = 20;
+let rScale = d3.scaleSqrt().domain([0, 210]).range([0, maxRadius]);
+
 function dataIsReady(data) {
   updateChart(data);
 }
 
 function updateChart(eventData) {
+  layout(eventData);
+
   d3.select('svg g.chart')
     .selectAll('circle')
     .data(eventData)
     .join('circle')
     .attr('cx', function(d, i) {
-      return i * 5;
+      // return i * 5;
+      return d.layout.x;
     })
-    .attr('cy', 50)
+    // .attr('cy', 50)
+    .attr('cy', function(d) {
+      return d.layout.y;
+    })
     .attr('r', function(d) {
-      return +d.Entrants;
+      //return rScale(+d.Entrants);
+      return d.layout.entrantsRadius;
     })
+    // .style('opacity', 0.1);
+    .style('fill', '#aaa');
+}
+
+function layout(data) {
+  let cellSize = 80, numCols = 10;
+
+  data.forEach(function(d) {
+    d.layout = {};
+
+    let i = d.Year - 1900;
+
+    let col = i % numCols;
+    d.layout.x = col * cellSize + 0.5 * cellSize;
+
+    let row = Math.floor(i / numCols);
+    d.layout.y = row * cellSize + 0.5 * cellSize;
+
+    d.layout.entrantsRadius = rScale(d.Entrants);
+  });
 }
